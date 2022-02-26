28bddfeca3f5e4901adbbe333b049356f515f3c4
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-02-26 16:12:16 +0100

    err handling with match

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,17 @@ fn main() {
     // Reading the file using the std::fs::read_to_string library, with an argument
     // equal to the args.path passed by the user
     // Here we are borrowing the args.path variable.
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path);
+    match content {
+        Ok(content) => { println!("File content {}", content); },
+        Err(error) => { println!("Error! {}", error); },
+    }
+
     // Iterate over the content, line by line, when a line is matching the pattern, display it.
     for line in content.lines()  {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
-    
+
 }
